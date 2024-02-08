dc2d41b77bb54d5fee16bf4bf45670de775f6b8d
Greg Pappas <gnpappas@proton.me>
2024-02-08 15:40:44 -0800

    Consolidate styling

--- src/style.ts
+++ src/style.ts
@@ -1,6 +1,4 @@
 export const ROOT_STYLE =
   "prose mx-auto bg-zinc-300 prose-hr:border-2 prose-hr:border-black prose-hr:m-0 sm:px-0 px-4 prose-p:mb-4 prose-headings:my-2 prose-a:decoration-2";
-export const POST_TITLE =
-  "flex sm:flex-row flex-col justify-between items-center";
-export const BLOG_ENTRIES =
+export const RESPONSIVE =
   "flex sm:flex-row flex-col justify-between items-center";
