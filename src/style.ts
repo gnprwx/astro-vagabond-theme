580c2e46150f9ba58c252f64226eb569ab0fbdc5
Greg Pappas <gnpappas@proton.me>
2024-02-08 20:00:23 -0800

    Consolidate styling

--- src/style.ts
+++ src/style.ts
@@ -2,4 +2,4 @@ export const ROOT_STYLE =
   "prose mx-auto bg-zinc-300 prose-hr:border-2 prose-hr:border-black prose-hr:m-0 sm:px-0 px-4 prose-p:mb-4 prose-headings:my-2 prose-a:decoration-2";
 export const RESPONSIVE =
   "flex sm:flex-row flex-col justify-between items-center";
-export const NAV_LINKS = "flex justify-center gap-5 text-xl";
+export const NAV_LINKS = "flex justify-center gap-5 text-xl my-4";
